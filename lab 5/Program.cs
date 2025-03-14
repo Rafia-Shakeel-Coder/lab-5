b4d009ba7d57da6dbb7ae6f595e10c62720139c9
Rafia Shakeel <24ntucsfl1209@student.ntu.edu.pk>
2025-03-14 23:41:13 +0500

    lab 5

--- lab 5/Program.cs
+++ lab 5/Program.cs
@@ -11,17 +11,20 @@ namespace lab_5
         static void Main(string[] args)
         {
             Sopping_cart cart = new Sopping_cart(1);
-            cart.customername = "Alice";
 
-            Product p1 = new Product(1, "Laptop", 1200.50);
+         
+            cart.customername = "RAFIA";
+          
+           Product p1 = new Product(1, "WATCH", 1200.50);
+
             Product p2 = new Product(2, "Mouse", 25.75);
-
             cart.Addproduct(p1);
             cart.Addproduct(p2);
-
             cart.DisplayCart();
 
             cart.RemoveProduct(2);
+
+            
             cart.DisplayCart();
         }
     }
